d28127eabe16036ea9e01d4e51bbf19017c928d4
StrafeAE <StrafeAE@gmail.com>
2025-02-25 15:11:47 -0500

    Update dash4.py

--- dash4.py
+++ dash4.py
@@ -8,4 +8,11 @@ df = pd.read_csv("customer_data.csv")
 # Let's get a KPI for Average Purchase Value
 # We can add a pie chart for Preferred Payment Method
 
+# KPI
 st.metric("Average Purchase Value:", f"${df['AveragePurchaseValue'].mean():,.2f}")
+
+# Pie Chart
+st.subheader("Preferred Payment Method")
+fig = px.pie(df, names='PreferredPaymentMethod')
+fig.update_traces(text = df['PreferredPaymentMethod'], textposition='outside')
+st.plotly_chart(fig, use_container_width = True)
