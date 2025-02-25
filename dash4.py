125d9a54624120e341fa568fd6b6dcc0185ada01
StrafeAE <StrafeAE@gmail.com>
2025-02-25 15:07:27 -0500

    Update dash4.py

--- dash4.py
+++ dash4.py
@@ -5,3 +5,7 @@ import pandas as pd
 st.title("Customer Data")
 df = pd.read_csv("customer_data.csv")
 
+# Let's get a KPI for Average Purchase Value
+# We can add a pie chart for Preferred Payment Method
+
+st.metric("Average Purchase Value:", f"${df['AveragePurchaseValue'].average():,.2f}")
